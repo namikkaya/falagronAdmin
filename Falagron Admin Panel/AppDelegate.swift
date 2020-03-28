4d87af219cf846b604f0ab65976fe8c4cf3222ba
namikkaya <namikkaya86@gmail.com>
2020-03-28 13:07:45 +0300

    Initial Commit

---
+++ Falagron Admin Panel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Falagron Admin Panel
+//
+//  Created by namik kaya on 28.03.2020.
+//  Copyright © 2020 namikkaya86@gmail.com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Falagron Admin PanelTests/Falagron_Admin_PanelTests.swift
@@ -0,0 +1,34 @@
+//
+//  Falagron_Admin_PanelTests.swift
+//  Falagron Admin PanelTests
+//
+//  Created by namik kaya on 28.03.2020.
+//  Copyright © 2020 namikkaya86@gmail.com. All rights reserved.
+//
+
+import XCTest
+@testable import Falagron_Admin_Panel
+
+class Falagron_Admin_PanelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
